5072d7f8a76caad5ac3a259e721d2ce0159c707e
Chesterkxng <cgoita00@gmail.com>
2024-01-22 00:36:25 +0000

    pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# 4. Client configuration file (w/ Puppet)
+file_line {
+	  ensure	=> 'present',
+	  path	=> '/etc/ssh/ssh_config',
+	  line	=> '	PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+	  ensure     => 'present',
+	  path   => '/etc/ssh/ssh_config',
+	  path	=> '	IdentityFile ~/.ssh/school',
+}
