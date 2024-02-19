4b1926ad74e72ed42c90555a8d4aee7bef216a00
Chesterkxng <cgoita00@gmail.com>
2024-02-19 17:13:40 +0000

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given user ID
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    # Accept the user Id from the command line arguments
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
