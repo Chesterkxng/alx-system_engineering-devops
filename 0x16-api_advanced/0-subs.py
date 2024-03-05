0bcf516e48b5a4c210c09ca94298e6453879ab74
Chesterkxng <cgoita00@gmail.com>
2024-03-05 19:26:00 +0000

    try/except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,13 @@ def number_of_subscribers(subreddit):
     """ Return the numbers of subreddit subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
+    except requests.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
